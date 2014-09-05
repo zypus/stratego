8df0d4ce61a5a64e7ea6e41296ba0c77434eff98
Fabian Fränz <f.fraenz@t-online.de>
2014-09-05 14:11:51 +0200

    LibGDX setup and lombok.

---
+++ desktop/src/com/theBombSquad/stratego/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.theBombSquad.stratego.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theBombSquad.stratego.Stratego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Stratego(), config);
+	}
+}
