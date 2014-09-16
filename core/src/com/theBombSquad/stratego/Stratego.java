d0a0cbc754c0dc62ff6ddeffbb2aaecede8b7451
Fabian Fränz <f.fraenz@t-online.de>
2014-09-16 14:40:49 +0200

    Sparse setup content. A lot TODOs.

--- core/src/com/theBombSquad/stratego/Stratego.java
+++ core/src/com/theBombSquad/stratego/Stratego.java
@@ -3,32 +3,67 @@ package com.theBombSquad.stratego;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.theBombSquad.stratego.gameMechanics.Game;
+import com.theBombSquad.stratego.gameMechanics.GameView;
+
+import static com.theBombSquad.stratego.StrategoConstants.ASSUMED_WINDOW_WIDTH;
 
 /**
- * TODO Add description
+ * Entry point for the stratego game. Setups everything and establishes remote connections if necessary.
  *
  * @author Fabian Fränz <f.fraenz@t-online.de>
  * @author Flo
+ *
+ * @version 1.0
+ * @created 10.09.14
+ *
+ * @date 16.09.14
+ *
+ * @log
+ * - Skeleton	10.09.14
+ * - TODOs 16.09.14
  */
 
 public class Stratego extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private float windowScale;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// TODO setup everything
+		windowScale = Gdx.graphics.getWidth() / ASSUMED_WINDOW_WIDTH;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// TODO call the appropriate renderer
+	}
+
+	private void setupMainMenu() {
+		// TODO create and show the main menu
+		// TODO listen for main menu completion
+	}
+
+	private void setupGame() {
+		// TODO create the players or get the players?
+		// TODO create the game instance
+		Game game = null;
+		// creates the two game views, one for each player perspective
+		GameView playerOneView = new GameView(game, StrategoConstants.PlayerID.PLAYER_1);
+		GameView playerTwoView = new GameView(game, StrategoConstants.PlayerID.PLAYER_2);
+		// TODO setup renderers
+		// TODO start the setup phase of the game
+	}
+
+	private void setupRenderer(GameView gameView1, GameView gameView2) {
+		// TODO setup the render system
+	}
+
+	private void listenForRemoteGameCreation() {
+		// TODO wait for an remote game creation on a yet to be defined socket
+		// TODO perform the necessary steps to connect and keep the game in sync
 	}
 }
