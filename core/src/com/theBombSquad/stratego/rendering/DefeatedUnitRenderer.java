2c0a499449c47d0beb3636d6e37f7a9c1008c3ed
Dude <Florian221@gmx.net>
2014-09-19 15:04:41 +0200

    Added Images of Units as well as a font.

--- core/src/com/theBombSquad/stratego/rendering/DefeatedUnitRenderer.java
+++ core/src/com/theBombSquad/stratego/rendering/DefeatedUnitRenderer.java
@@ -1,7 +1,11 @@
 package com.theBombSquad.stratego.rendering;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.theBombSquad.stratego.StrategoConstants;
 import static com.theBombSquad.stratego.StrategoConstants.*;
 
@@ -13,14 +17,33 @@ import static com.theBombSquad.stratego.StrategoConstants.*;
  */
 public class DefeatedUnitRenderer extends Renderer {
 	
+	/** Reference to the font in Render Data */
+	private BitmapFont font;
+	
 	/** Textures of Units that have been defeated, in the order they will be drawn from top to bottom */
 	private TextureRegion[] defeatedUnits;
 	
+	/** Textures of Units's backs that have been defeated, in the order they will be drawn from top to bottom */
+	private TextureRegion[] defeatedBacks;
+	
 	@Override
 	public void init() {
+		initUnitImages();
+		this.font = super.renderData.getFont();
+	}
+	
+	/** Initializes the Texture Regions that represent the Units */
+	private void initUnitImages(){
 		defeatedUnits = new TextureRegion[11];
+		Array<AtlasRegion> units = super.renderData.getAtlas().findRegions("unit");
 		for(int c=0; c<defeatedUnits.length; c++){
-			defeatedUnits[c] = super.renderData.getAtlas().findRegion("tileWater");
+			defeatedUnits[c] = units.get(c+1);
+			
+		}
+		defeatedBacks = new TextureRegion[2];
+		Array<AtlasRegion> backs = super.renderData.getAtlas().findRegions("back");
+		for(int c=0; c<defeatedBacks.length; c++){
+			defeatedBacks[c] = backs.get(c);
 		}
 	}
 	
@@ -28,12 +51,27 @@ public class DefeatedUnitRenderer extends Renderer {
 	public void render(SpriteBatch batch) {
 		float size = POINT_TILE_SIZE*getScale();
 		float yStart = 1.5f*POINT_TILE_SIZE;
+		font.setScale(getScale());
+		drawUnits(batch, size, yStart);
+	}
+	
+	/** Renders Unit Images */
+	private void drawUnits(SpriteBatch batch, float size, float yStart){
 		for(int cp=0; cp<2; cp++){
 			float x = ((ASSUMED_WINDOW_WIDTH-(POINT_TILE_SIZE*1.5f))*cp + POINT_TILE_SIZE*0.25f)*getScale();
 			for(int cu=0; cu<defeatedUnits.length; cu++){
-				batch.draw(defeatedUnits[cu], x, (((float)ASSUMED_WINDOW_HEIGHT-yStart)-(cu*POINT_TILE_SIZE))*getScale(), size, size);
+				float realX = x;
+				float realY = (((float)ASSUMED_WINDOW_HEIGHT-yStart)-(cu*POINT_TILE_SIZE))*getScale();
+				batch.draw(defeatedBacks[cp], realX, realY, size, size);
+				batch.draw(defeatedUnits[cu], realX, realY, size, size);
+				font.draw(batch, ""+calcDead(cu, cp), realX+POINT_TILE_SIZE*getScale()*0.7f, realY+font.getCapHeight()*1.5f);
 			}
 		}
+	}
+	
+	/** Returns the amount of Units of given type and player that have already died */
+	private int calcDead(int unit, int player){
+		return 0;
 	}
 	
 }
--- core/src/com/theBombSquad/stratego/rendering/RenderData.java
+++ core/src/com/theBombSquad/stratego/rendering/RenderData.java
@@ -3,6 +3,9 @@ package com.theBombSquad.stratego.rendering;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 /** 
@@ -11,13 +14,22 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
  *
  */
 @Data
-@AllArgsConstructor
 public class RenderData {
+	
+	/** Construct New */
+	public RenderData(float scale, TextureAtlas atlas){
+		this.scale = scale;
+		this.atlas = atlas;
+		this.font = new BitmapFont(Gdx.files.internal("atlas/font.fnt"));
+	}
 	
 	/** Scale to which everything will be drawn */
 	private float scale;
 	
 	/** Texture Atlas containing all images */
 	private TextureAtlas atlas;
+	
+	/** The font used for rendering all text */
+	private BitmapFont font;
 	
 }
