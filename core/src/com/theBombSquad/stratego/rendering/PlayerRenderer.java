49315950d9e571b89bd26eba2f9cd00f2ec1ce17
Fabian Fränz <f.fraenz@t-online.de>
2014-10-27 17:09:07 +0100

    Added tween engine.

---
+++ core/src/com/theBombSquad/stratego/rendering/PlayerRenderer.java
@@ -0,0 +1,10 @@
+package com.theBombSquad.stratego.rendering;
+
+/**
+ * TODO Add description
+ *
+ * @author Fabian Fraenz <f.fraenz@t-online.de>
+ * @created $(DATE)
+ */
+public class PlayerRenderer {
+}
