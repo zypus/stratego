e4e46b2e8ad4ee73404277062cb0739edd38b228
Sharonneke95 <sharonneke95@hotmail.com>
2014-11-12 11:43:40 +0100

    Flagtactic1 done

--- core/src/com/theBombSquad/stratego/player/ai/setup/FlagTactic.java
+++ core/src/com/theBombSquad/stratego/player/ai/setup/FlagTactic.java
@@ -1,28 +1,58 @@
 package com.theBombSquad.stratego.player.ai.setup;
+
 import java.util.ArrayList;
 
 import com.theBombSquad.stratego.gameMechanics.board.Setup;
+import com.theBombSquad.stratego.gameMechanics.board.Unit.UnitType;
 
-public class FlagTactic extends Tactic{
+public class FlagTactic extends Tactic {
 	protected ArrayList<Strategy> strategies;
-	
-	//this just stores the setup changed by extending it flag tactics 
-	// extending it class have to state whether it might be offensive def or middle
-	public FlagTactic(){
+
+	// this just stores the setup changed by extending it flag tactics
+	// extending it class have to state whether it might be offensive def or
+	// middle
+	public FlagTactic() {
 		super();
 	}
-	public FlagTactic(AISetup setup){
+
+	public FlagTactic(AISetup setup) {
 		super(setup);
-		this.setup=setup;
+		this.setup = setup;
 	}
-	public AISetup getSetup(){
+
+	public AISetup getSetup() {
 		return setup;
 	}
-	public ArrayList<Strategy> getStrategies(){
+
+	public ArrayList<Strategy> getStrategies() {
 		return strategies;
 	}
-	
-	
-	
 
+	public UnitType randomizeSL() {
+		double random = Math.random();
+		if (random <= 0.5) {
+			for (int i = 0; i < setup.getView().getAvailableUnits().size(); i++) {
+				if (setup.getView().getAvailableUnits().get(i).getType() == UnitType.SERGEANT) {
+					return UnitType.SERGEANT;
+				}
+			}
+			return UnitType.LIEUTENANT;
+		}
+		for (int i = 0; i < setup.getView().getAvailableUnits().size(); i++) {
+			if (setup.getView().getAvailableUnits().get(i).getType() == UnitType.LIEUTENANT) {
+				return UnitType.LIEUTENANT;
+			}
+		}
+		return UnitType.SERGEANT;
+
+	}
+
+	/*
+	 * strategy1: move(0, 0, 6 ,10);
+	 * 
+	 * make new method to place?
+	 * 
+	 * UnitPlacement toPut = new UnitPlacement(UnitType.FLAG, 6, 10, 10);
+	 * super.placeUnit(toPut);
+	 */
 }
