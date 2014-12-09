751b2ea241858080bad2b59b28ee514fd23d4c3b
Fabian Fränz <f.fraenz@t-online.de>
2014-12-09 11:02:51 +0100

    Work in progress.

--- core/src/com/theBombSquad/stratego/player/ai/players/TDStratego/TDStratego.java
+++ core/src/com/theBombSquad/stratego/player/ai/players/TDStratego/TDStratego.java
@@ -1,9 +1,15 @@
 package com.theBombSquad.stratego.player.ai.players.TDStratego;
 
+import Jama.Matrix;
+import com.theBombSquad.stratego.StrategoConstants;
 import com.theBombSquad.stratego.gameMechanics.Game;
 import com.theBombSquad.stratego.gameMechanics.board.Move;
 import com.theBombSquad.stratego.gameMechanics.board.Setup;
 import com.theBombSquad.stratego.player.ai.AI;
+import com.theBombSquad.stratego.player.ai.schrodingersBoard.SchrodingersBoard;
+import com.theBombSquad.stratego.player.ai.schrodingersBoard.SchrodingersUnit;
+
+import static com.theBombSquad.stratego.StrategoConstants.PlayerID.*;
 
 /**
  * TODO Add description
@@ -26,6 +32,41 @@ public class TDStratego
 	@Override
 	protected Setup setup() {
 		return null;
+	}
+
+	private class TDPlayer extends AbstractTDPlayer<SchrodingersBoard> {
+
+		private final int UNIT_TYPE_COUNT = 12; /** The ten ranks plus bomb and flag*/
+		private final int PLAYER_FLAG = 2; /** Owner of the unit*/
+		private final int NUMBER_OF_LAKES = 8;
+
+		@Override
+		protected Matrix stateToActivation(SchrodingersBoard state) {
+			int infoSize = (state.getWidth() * state.getHeight() - NUMBER_OF_LAKES) * (UNIT_TYPE_COUNT + PLAYER_FLAG)
+						   + PLAYER_FLAG * UNIT_TYPE_COUNT
+						   + PLAYER_FLAG;
+			Matrix activation = new Matrix(infoSize, 1);
+			int index = 0;
+			for (int x = 0; x < state.getWidth(); x++) {
+				for (int y = 0; y < state.getHeight(); y++) {
+					SchrodingersUnit unit = state.getUnit(x, y);
+					if (!unit.isLake()) {
+						// owner
+						activation.set(index++, 1, (unit.getOwner() == PLAYER_1) ? 1 : 0);
+						activation.set(index++, 1, (unit.getOwner() == PLAYER_2) ? 1 : 0);
+						// unit probabilities
+						for ()
+					}
+
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected float utilityValue(SchrodingersBoard state, Matrix output) {
+			return 0;
+		}
 	}
 
 }
--- core/src/com/theBombSquad/stratego/player/ai/schrodingersBoard/SchrodingersBoard.java
+++ core/src/com/theBombSquad/stratego/player/ai/schrodingersBoard/SchrodingersBoard.java
@@ -1,16 +1,14 @@
 package com.theBombSquad.stratego.player.ai.schrodingersBoard;
 
-import java.util.ArrayList;
-
-import lombok.Data;
-import lombok.Getter;
-
 import com.theBombSquad.stratego.StrategoConstants.PlayerID;
 import com.theBombSquad.stratego.gameMechanics.Game.GameView;
 import com.theBombSquad.stratego.gameMechanics.board.GameBoard;
 import com.theBombSquad.stratego.gameMechanics.board.Unit;
 import com.theBombSquad.stratego.gameMechanics.board.Unit.UnitType;
 import com.theBombSquad.stratego.player.ai.evaluationFunction.EvaluationFunction;
+import lombok.Data;
+
+import java.util.ArrayList;
 
 @Data
 /** This Class is supposed to simplify and abstract board states and board state manipulation for unknown units */
@@ -20,7 +18,7 @@ public class SchrodingersBoard {
 	private int opponentArmySize;
 	private int ownArmySize;
 	private GameView view;
-	
+
 	/** Constructs New Schrodingers Box Which Is Dependent On A Single (The Current) State */
 	public SchrodingersBoard(GameView view){
 		this.view = view;
@@ -77,8 +75,8 @@ public class SchrodingersBoard {
 			}
 		}
 	}
-	
-	
+
+
 	/** Constructor Used for cloning */
 	public SchrodingersBoard(SchrodingersUnit[][] board, int ownArmySize, int opponentArmySize, GameView view){
 		this.board = board;
@@ -86,7 +84,7 @@ public class SchrodingersBoard {
 		this.opponentArmySize = opponentArmySize;
 		this.view = view;
 	}
-	
+
 	public SchrodingersBoard clone(){
 		SchrodingersUnit[][] cBoard = new SchrodingersUnit[board.length][board[0].length];
 		for(int cy=0; cy<cBoard.length; cy++){
@@ -96,7 +94,7 @@ public class SchrodingersBoard {
 		}
 		return new SchrodingersBoard(cBoard, ownArmySize, opponentArmySize, view);
 	}
-	
+
 	/** Creates a new Board in which unit from origin is now at destination */
 	public ArrayList<SchrodingersBoard> moveUnit(int originX, int originY, int destX, int destY){
 		ArrayList<SchrodingersBoard> list = new ArrayList<SchrodingersBoard>();
@@ -162,21 +160,46 @@ public class SchrodingersBoard {
 		}
 		return list;
 	}
-	
+
 	/** Generates All Possible Boards That Can Follow This Board Given That Given Player Makes A Move */
 	public SchrodingersBoard[] generateAllMoves(PlayerID player){
 		return new SchrodingersBoard[]{this};
 	}
-	
+
 	/** Evaluates This Board */
 	public float evaluate(EvaluationFunction eval){
 		return 42f;
 	}
-	
+
 	/** Generates A Random Move Based Upon The Given Player */
 	public SchrodingersBoard generateRandomMove(PlayerID player){
 		return this;
 	}
-	
-	
+
+	/**
+	 * Gets the Schroedinger unit at the specified position.
+	 * @param x X coordinate of the unit.
+	 * @param y Y coordinate of the unit.
+	 * @return The Schroedinger unit.
+	 */
+	public SchrodingersUnit getUnit(int x, int y) {
+		return board[y][x];
+	}
+
+	/**
+	 * Gets the width of the board.
+	 * @return The width.
+	 */
+	public int getWidth() {
+		return board[0].length;
+	}
+
+	/**
+	 * Gets the height of the board.
+	 * @return The height.
+	 */
+	public int getHeight() {
+		return board.length;
+	}
+
 }
