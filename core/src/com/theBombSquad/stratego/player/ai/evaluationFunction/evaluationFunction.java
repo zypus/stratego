615121b71f2137e77be52850705e7a9ace3abd98
Fabian Fränz <f.fraenz@t-online.de>
2014-12-10 11:01:34 +0100

    Dafuq.

--- core/src/com/theBombSquad/stratego/player/ai/evaluationFunction/evaluationFunction.java
+++ core/src/com/theBombSquad/stratego/player/ai/evaluationFunction/evaluationFunction.java
@@ -1,11 +1,11 @@
 package com.theBombSquad.stratego.player.ai.evaluationFunction;
 
+import com.theBombSquad.stratego.StrategoConstants;
 import com.theBombSquad.stratego.gameMechanics.board.GameBoard;
-import com.theBombSquad.stratego.player.ai.AIGameState;
 
 /** Interface for Evaluation Functions */
 public interface EvaluationFunction {
-	
-	public float evaluate(GameBoard gamestate, PlayerID player);
+
+	public float evaluate(GameBoard state, StrategoConstants.PlayerID player);
 
 }
