707ee2a76c50b3fcd2ff01c58d48e316303a868b
Flo <Florian221@gmx.net>
2015-01-14 16:49:39 +0100

    Improvements

--- core/src/com/theBombSquad/stratego/player/ai/players/planner/PlanPunishUnitReveals.java
+++ core/src/com/theBombSquad/stratego/player/ai/players/planner/PlanPunishUnitReveals.java
@@ -39,7 +39,7 @@ public class PlanPunishUnitReveals implements Plan{
 		}
 		
 		//As This Is Punishment The Value Should Be Subtracted From Complete
-		return 0;//-revealHiddenUnitPenalty;
+		return revealHiddenUnitPenalty;
 	}
 
 	@Override
