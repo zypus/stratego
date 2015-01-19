e1374a5b5672293a2994d60377fa101e37501b26
Fabian Fränz <f.fraenz@t-online.de>
2015-01-19 18:12:00 +0100

    Removed printlns.

--- core/src/com/theBombSquad/stratego/player/ai/setup/FlagTactics/FlagTactic11.java
+++ core/src/com/theBombSquad/stratego/player/ai/setup/FlagTactics/FlagTactic11.java
@@ -1,18 +1,18 @@
 package com.theBombSquad.stratego.player.ai.setup.FlagTactics;
 
-import java.util.ArrayList;
-
 import com.theBombSquad.stratego.gameMechanics.board.Unit.UnitType;
 import com.theBombSquad.stratego.player.ai.setup.AISetup;
 import com.theBombSquad.stratego.player.ai.setup.FlagTactic;
-import com.theBombSquad.stratego.player.ai.setup.Strategy;
-import com.theBombSquad.stratego.player.ai.setup.UnitPlacement;
 import com.theBombSquad.stratego.player.ai.setup.Strategies.Defensive;
 import com.theBombSquad.stratego.player.ai.setup.Strategies.Middle;
 import com.theBombSquad.stratego.player.ai.setup.Strategies.Ofensive;
+import com.theBombSquad.stratego.player.ai.setup.Strategy;
+import com.theBombSquad.stratego.player.ai.setup.UnitPlacement;
+
+import java.util.ArrayList;
 
 public class FlagTactic11 extends FlagTactic{
-	
+
 	public FlagTactic11(AISetup setup) {
 		super(setup);
 		proceed();
@@ -36,7 +36,7 @@ public class FlagTactic11 extends FlagTactic{
 	}
 
 	private void proceed() {
-		
+
 		ArrayList<UnitType> units = new ArrayList<UnitType>();
 		units.add(UnitType.BOMB);
 		units.add(UnitType.BOMB);
@@ -46,27 +46,27 @@ public class FlagTactic11 extends FlagTactic{
 		if( order.get(0) == UnitType.MARSHAL){
 			blockMarshal(y);
 		}
-		else{ 
+		else{
 			blockBomb(y);
 		}
 		y = 4;
 		if(order.get(1) == UnitType.MARSHAL){
 			blockMarshal(y);
 		}
-		else{ 
+		else{
 			blockBomb(y);
 		}
 		y = 8;
 		if(order.get(2) == UnitType.MARSHAL){
 			blockMarshal(y);
 		}
-		else{ 
+		else{
 			blockBomb(y);
 		}
 		blockFlag();
-		
+
 	}
-	
+
 	private void blockMarshal(int y){
 		ArrayList<UnitType> units = new ArrayList<UnitType>();
 		units.add(UnitType.MARSHAL);
@@ -74,7 +74,7 @@ public class FlagTactic11 extends FlagTactic{
 		units.add(UnitType.COLONEL);
 		units.add(UnitType.MAJOR);
 		ArrayList<UnitType> order = super.giveOrder(units);
-		
+
 		UnitPlacement toPut = new UnitPlacement(order.get(0), y,0, 1);
 		super.placeUnit(toPut);
 		toPut = new UnitPlacement(order.get(1),  y+1,0, 1);
@@ -83,7 +83,7 @@ public class FlagTactic11 extends FlagTactic{
 		super.placeUnit(toPut);
 		toPut = new UnitPlacement(order.get(3), y, 1, 1);
 		super.placeUnit(toPut);
-		
+
 		//SPY
 		int [][] spots = {
 				{y-1, 0 , 1},
@@ -106,52 +106,52 @@ public class FlagTactic11 extends FlagTactic{
 		toPut = super.randomizeUnitPlacement();
 		super.placeUnit(toPut);
 	}
-	
+
 	private void blockBomb(int y){
 		UnitPlacement toPut = new UnitPlacement(UnitType.BOMB, y, 0, 1);
 		super.placeUnit(toPut);
 		toPut.setX(y+1);
 		super.placeUnit(toPut);
 	}
-	
+
 	private void blockFlag(){
 		super.empty();
 		UnitPlacement toPut;
 		for(int i = 0 ; i < setup.getWidth() ; i++ ){
 			if(i <= 3 || i >= 6){
-				System.out.println("IF!");
+//				System.out.println("IF!");
 				possiblePlacements.add(new UnitPlacement(UnitType.FLAG, i, 3, 2));
 			}
 			else{
-				System.out.println("IF@");
+//				System.out.println("IF@");
 				possiblePlacements.add(new UnitPlacement(UnitType.FLAG, i, 3, 1));
 			}
 		}
 		toPut = super.randomizeUnitPlacement();
 		super.placeUnit(toPut);
 		int x = toPut.getX();
-		System.out.println("x= " + x);
+//		System.out.println("x= " + x);
 		toPut.setUnitType(UnitType.BOMB);
 		if(super.isFree(x, 2)){
-			System.out.println("CHECK1");
+//			System.out.println("CHECK1");
 			toPut.setX(x);
 			toPut.setY(2);
 			super.placeUnit(toPut);
 		}
 		if(super.isFree(x+1,3)){
-			System.out.println("CHECK2");
+//			System.out.println("CHECK2");
 			toPut.setX(x+1);
 			toPut.setY(3);
 			super.placeUnit(toPut);
 		}
 		if(super.isFree(x-1,3)&& super.hasAvailable(UnitType.BOMB)){
-			System.out.println("CHECK3");
+//			System.out.println("CHECK3");
 			toPut.setX(x-1);
 			toPut.setY(3);
 			super.placeUnit(toPut);
 		}
 		if(super.hasAvailable(UnitType.BOMB)){
-			System.out.println("CHECK4");
+//			System.out.println("CHECK4");
 			if(super.isFree(x+1,2)){
 				toPut.setX(x+1);
 				toPut.setY(2);
